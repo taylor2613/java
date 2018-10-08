486c565ee29df6609f90c702113a44039ff14e96
taylor2613 <43841893+taylor2613@users.noreply.github.com>
2018-10-09 08:28:23 +0900

    Add files via upload

---
+++ hellojava.java
@@ -0,0 +1,20 @@
+package org.opentutorials.javatutorilas.eclipse;
+
+public class hellojava {
+
+	public static void main(String[] args) {
+		int a;
+		a=1;
+		System.out.println(a+1);
+		
+		a=2;
+		System.out.println(a+1);
+		
+		double b;
+		
+		b=1.1;
+		System.out.println(b+1.1);
+		
+	}
+
+}
